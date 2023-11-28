b7df24654c8c414cef262d76025c8d4613d5760a
engel <engel.encarnacion.7e5@itb.cat>
2023-11-28 10:11:44 +0100

    f

---
+++ e1.py
@@ -0,0 +1,10 @@
+"""
+Gabriel Ghiorghita / Engel Encarnación / Oscar Cordoba
+10/10/2023
+ASIX1c M03 UF1
+Descripción: Numeros positivos y negativos
+"""
+
+primernumero=int(input("introduce un numero"))
+primernumero=int(input("introduce un numero"))
+primernumero=int(input("introduce un numero"))
