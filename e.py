b8322c30b72723238dddeb703f769a5f02b0a7c3
gabriel.ghiorghita.7e7 <gabriel.ghiorghita.7e7@itb.cat>
2023-11-28 11:31:34 +0100

    s

--- e.py
+++ e.py
@@ -1,1 +1,26 @@
 
+"""
+Gabriel Razvan Ghiorghita
+23/11/2023
+ASIXc1C M03 UF1
+Activitat: Piramid
+
+"""
+def imprimir_piramide(numero):
+    for i in range(1, numero + 1):
+        print("#" * i)
+
+numero_str = input("Introdueix un número per a l'altura de la teva piràmide: ")
+
+numero = int(numero_str)
+
+print(imprimir_piramide(numero))
+print("Programa finalitzat, tancant...")
+
+
+#VERSIÓ CLASSE:
+
+#TOTXO = "#"
+#alçada = int(input())
+#for nivell in range (1,alçada+1):
+    #print(TOTXO * nivell)
--- e4.py
+++ e4.py
@@ -1,3 +1,9 @@
+"""
+Gabriel Ghiorghita / Engel Encarnación / Oscar Cordoba
+10/10/2023
+ASIX1c M03 UF1
+Descripción: Tauler d'escacs
+"""
 BLANC = "⬜"
 NEGRE = "⬛"
 
