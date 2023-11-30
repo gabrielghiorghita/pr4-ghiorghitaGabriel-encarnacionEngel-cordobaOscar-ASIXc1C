4643faa9194af511cc90ae634c109732ed1a9b7a
engel <engel.encarnacion.7e5@itb.cat>
2023-11-30 09:10:13 +0100

    s

--- e2.py
+++ e2.py
@@ -5,9 +5,10 @@ ASIX1c M03 UF1
 Descripción: Piràmide
 """
 def imprimir_piramide(numero):
-    for i in range(1, numero + 1):
+    if numero >= 9:
+     for i in range(1, numero + 1):
         if i == numero:
-            print(str(i) * (2* i- 1))
+            print(str(i) * (i))
         else:
             spaces = " " * (i - 1)
             if i == 1:
@@ -17,9 +18,11 @@ def imprimir_piramide(numero):
 
 
 numero_str = input("Introduce un número para la altura de tu pirámide: ")
-
 numero = int(numero_str)
 
-imprimir_piramide(numero)
+if numero <=9:
+    imprimir_piramide(numero)
+else:
+ print("El número debe estar en el rango de 2 a 9.")
 
 print("Programa finalizado, cerrando...")
