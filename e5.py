fa07be38e5e1a01376befffc7981435a4660e46e
gabriel.ghiorghita.7e7 <gabriel.ghiorghita.7e7@itb.cat>
2023-11-30 08:30:51 +0100

    ds

--- e5.py
+++ e5.py
@@ -5,24 +5,27 @@ ASIX1c M03 UF1
 Descripción: Multiplicació de dos nombres sencers mitjançant sumes.
 
 """
+def suma(a, b):
+    # Funció que suma dos nombres
+    return a + b
+
 def multiplicacio(a, b):
-    # Cas base: quan b és 0, el resultat és 0
-    if b == 0:
-        return 0
-    # Cas general: suma "a" i crida recursiva amb "a" i "b-1"
-    else:
-        return a + multiplicacio(a, b-1)
+    # Funció que realitza la multiplicació mitjançant sumes successives
+    resultat = 0
+    for _ in range(b):
+        resultat = suma(resultat, a)
+    return resultat
 
 # Programa principal
 if __name__ == "__main__":
-    # Demana a l'usuari dos nombres per multiplicar
+    # Demanem a l'usuari dos nombres per multiplicar
     num1 = int(input("Introdueix el primer nombre: "))
     num2 = int(input("Introdueix el segon nombre: "))
 
-    # Calcula la multiplicació utilitzant la funció
+    # Calculem la multiplicació utilitzant la funció
     resultat = multiplicacio(num1, num2)
 
-    # Mostra el resultat
+    # Mostrem el resultat
     print("El resultat de la multiplicació és:", resultat)
 
 
